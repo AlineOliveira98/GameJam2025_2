df4a89ae0e6dde75652078cd54e5726ec7a77932
Aline Oliveira <alineboliveira98@gmail.com>
2025-06-05 00:01:06 -0300

    scene update

---
+++ Assets/_Sources/Scripts/Audio/AudioController.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioSource sourceMusic;
+    [SerializeField] private AudioSource sourceSFX;
+    [SerializeField] private AudioClip menuMusic;
+    [SerializeField] private AudioClip gameplayMusic;
+
+    private float MusicVolume
+    {
+        get { return PlayerPrefs.GetFloat("MusicPref", 0.2f); }
+        set { PlayerPrefs.SetFloat("MusicPref", value); }
+    }
+
+    private float SFXVolume
+    {
+        get { return PlayerPrefs.GetFloat("SFXPref", 0.2f); }
+        set { PlayerPrefs.SetFloat("SFXPref", value); }
+    }
+
+    void Start()
+    {
+        PlayMusic(menuMusic);
+    }
+
+    public void PlaySFX(AudioClip sfx)
+    {
+        sourceSFX.PlayOneShot(sfx);
+    }
+
+    public void PlayMusic(AudioClip music)
+    {
+        sourceMusic.Stop();
+        sourceMusic.clip = music;
+        sourceMusic.Play();
+    }
+}
