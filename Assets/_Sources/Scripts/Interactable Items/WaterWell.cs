c7be0899eea2750985d475488c310a5adf163f39
Aline Oliveira <alineboliveira98@gmail.com>
2025-06-04 22:06:53 -0300

    water well teleport

--- Assets/_Sources/Scripts/Interactable Items/WaterWell.cs
+++ Assets/_Sources/Scripts/Interactable Items/WaterWell.cs
@@ -1,6 +1,16 @@
+using System;
 using UnityEngine;
 
 public class WaterWell : Interactable
 {
-    
+    [SerializeField] private WaterWell waterWellRef;
+    public Transform spawnArea;
+
+    public static Action<WaterWell> OnTeleportActivated;
+
+    public override void Interact()
+    {
+        base.Interact();
+        OnTeleportActivated.Invoke(waterWellRef);
+    }
 }
--- Assets/_Sources/Scripts/Player/PlayerMovement.cs
+++ Assets/_Sources/Scripts/Player/PlayerMovement.cs
@@ -23,6 +23,16 @@ public class PlayerMovement : MonoBehaviour
 
     public float speedMultiplier { get; private set; } = 1f;
 
+    private void OnEnable()
+    {
+        WaterWell.OnTeleportActivated += Teleport;
+    }
+
+    private void OnDisable()
+    {
+        WaterWell.OnTeleportActivated -= Teleport;
+    }
+
     private void Start()
     {
         dash = new BasicDash(player.rb, 50f, 0.03f, 0.5f, agent);
@@ -55,6 +65,14 @@ public class PlayerMovement : MonoBehaviour
     {
         if (movementLocked || !CanMove) return;
         MoveInput();
+    }
+
+    private void Teleport(WaterWell well)
+    {
+        agent.isStopped = true;
+        agent.ResetPath();
+        agent.Warp(well.spawnArea.position);
+        agent.isStopped = false;
     }
 
     private void MoveInput()
