996244f7aab16113f777e23ba387b6331a573e18
Aline Oliveira <alineboliveira98@gmail.com>
2025-06-04 21:52:01 -0300

    delay to player take damage

--- Assets/_Sources/Scripts/Player/PlayerHealth.cs
+++ Assets/_Sources/Scripts/Player/PlayerHealth.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
     [SerializeField] private float maxHealth;
-    [SerializeField] private float currentHealth = 0;
+    [SerializeField] private float delayToTakeDamage = 0.5f;
+    [SerializeField] private float delayToDie = 0.5f;
     [SerializeField] private bool receiveDamage = true;
+
+    private float currentHealth = 0;
 
     public bool IsDead { get; set; }
     public bool IsInvincible { get; private set; } = false;
@@ -27,7 +30,7 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         OnPlayerHealthChanged?.Invoke(currentHealth, maxHealth);
     }
 
-    public void TakeDamage(float damage)
+    public async void TakeDamage(float damage)
     {
         if (!receiveDamage) return;
         if (IsDead || IsInvincible) return;
@@ -35,10 +38,14 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         currentHealth -= damage;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
 
+        await Task.Delay(TimeSpan.FromSeconds(delayToTakeDamage));
+
         OnPlayerHealthChanged?.Invoke(currentHealth, maxHealth);
 
         if (currentHealth <= 0f)
         {
+            await Task.Delay(TimeSpan.FromSeconds(delayToDie));
+
             IsDead = true;
             GameController.Instance.GameOver();
             OnPlayerDied?.Invoke();
