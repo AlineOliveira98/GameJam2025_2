308ca730533c4a18e723883d58a6950ef24853b6
Aline Oliveira <alineboliveira98@gmail.com>
2025-06-06 13:44:42 -0300

    text animation

--- Assets/_Sources/Scripts/UI/GameplayUI.cs
+++ Assets/_Sources/Scripts/UI/GameplayUI.cs
@@ -1,9 +1,14 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
 public class GameplayUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI victimsCount;
+
+    [Header("Animation")]
+    [SerializeField] private float scaleFactor = 1.5f;
+    [SerializeField] private float speed = 1f;
 
     void OnEnable()
     {
@@ -20,6 +25,8 @@ public class GameplayUI : MonoBehaviour
     void Start()
     {
         UpdateVictimsCount();
+
+        victimsCount.transform.DOScale(Vector2.one * scaleFactor, speed).SetSpeedBased().SetLoops(-1, LoopType.Yoyo);
     }
 
     private void UpdateVictimsCount(NPC animal = null)
