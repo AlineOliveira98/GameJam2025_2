8aaba822fa2419d97e91d62252a72a3120b5c20f
GuilhermeSV7 <mr.guilhermelimasilva@gmail.com>
2025-06-08 16:52:33 -0300

    Parabola text

--- Assets/_Sources/Scripts/AnimatedGradientText.cs
+++ Assets/_Sources/Scripts/AnimatedGradientText.cs
@@ -2,11 +2,16 @@
 using UnityEngine;
 
 [RequireComponent(typeof(TMP_Text))]
-public class PerCharacterGradient : MonoBehaviour
+public class PerCharacterGradientWithParabola : MonoBehaviour
 {
+    [Header("Cores")]
     public float hueSpeed = 0.5f;
-    public float saturation = 1f;
-    public float brightness = 1f;
+    [Range(0f, 1f)] public float saturation = 1f;
+    [Range(0f, 1f)] public float brightness = 1f;
+
+    [Header("Curvatura")]
+    [Tooltip("Altura máxima da curva no centro do texto")]
+    public float curveStrength = 10f;
 
     private TMP_Text text;
     private TMP_TextInfo textInfo;
@@ -14,7 +19,6 @@ public class PerCharacterGradient : MonoBehaviour
     void Awake()
     {
         text = GetComponent<TMP_Text>();
-        text.ForceMeshUpdate();
     }
 
     void Update()
@@ -23,6 +27,10 @@ public class PerCharacterGradient : MonoBehaviour
         textInfo = text.textInfo;
 
         float time = Time.time * hueSpeed;
+        float boundsWidth = text.bounds.size.x;
+        float halfWidth = boundsWidth / 2f;
+
+        float parabolaFactor = curveStrength / (halfWidth * halfWidth);
 
         for (int i = 0; i < textInfo.characterCount; i++)
         {
@@ -32,19 +40,30 @@ public class PerCharacterGradient : MonoBehaviour
             int vertexIndex = charInfo.vertexIndex;
             int materialIndex = charInfo.materialReferenceIndex;
 
-            var vertices = textInfo.meshInfo[materialIndex].colors32;
+            var vertices = textInfo.meshInfo[materialIndex].vertices;
+            var colors = textInfo.meshInfo[materialIndex].colors32;
+
+            Vector3 charMid = (vertices[vertexIndex + 0] + vertices[vertexIndex + 2]) / 2;
+
+            float x = charMid.x;
+
+            float curveY = -parabolaFactor * x * x + curveStrength;
+
+            Vector3 offset = new Vector3(0, curveY, 0);
+
+            for (int j = 0; j < 4; j++)
+                vertices[vertexIndex + j] += offset;
 
             float hue = Mathf.Repeat(time + (i * 0.05f), 1f);
             Color32 c = Color.HSVToRGB(hue, saturation, brightness);
 
-            vertices[vertexIndex + 0] = c;
-            vertices[vertexIndex + 1] = c;
-            vertices[vertexIndex + 2] = c;
-            vertices[vertexIndex + 3] = c;
+            for (int j = 0; j < 4; j++)
+                colors[vertexIndex + j] = c;
         }
 
         for (int i = 0; i < textInfo.meshInfo.Length; i++)
         {
+            textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
             textInfo.meshInfo[i].mesh.colors32 = textInfo.meshInfo[i].colors32;
             text.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
         }
